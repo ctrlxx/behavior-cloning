f38b197ad1d893d2996d1edf80d2dd31b2bed9b7
czacker <1209892890@qq.com>
2018-11-20 00:52:01 +0800

    update new model.h5 and writeup

--- model.py
+++ model.py
@@ -42,16 +42,18 @@ x_train = np.array(augmented_images)
 y_train = np.array(augmented_measurements)
 
 from keras.models import Sequential
-from keras.layers import Flatten, Dense, Lambda, Convolution2D
+from keras.layers import Flatten, Dense, Lambda, Convolution2D, Dropout
 from keras.layers import Cropping2D
 
 model = Sequential()
 model.add(Lambda(lambda x: x / 255.0 - 0.5, input_shape=(160, 320, 3)))
 model.add(Cropping2D(cropping=((70, 25), (0, 0))))
 model.add(Convolution2D(24, 5, 5, subsample=(2, 2), activation='relu'))
+model.add(Dropout(0.5))
 model.add(Convolution2D(36, 5, 5, subsample=(2, 2), activation='relu'))
+model.add(Dropout(0.5))
 model.add(Convolution2D(48, 5, 5, subsample=(2, 2), activation='relu'))
-
+model.add(Dropout(0.5))
 model.add(Convolution2D(64, 3, 3, activation='relu'))
 model.add(Convolution2D(64, 3, 3, activation='relu'))
 
